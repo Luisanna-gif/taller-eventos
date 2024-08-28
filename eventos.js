fac53cd95bd9cbdc81be55fad18cbacca348e49b
Luisanna-gif <barralesluli.2002@gmail.com>
2024-08-27 23:55:14 -0300

    Modificaciones finales

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('ElDiv').addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
+
